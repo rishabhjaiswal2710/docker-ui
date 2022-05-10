621c9153ef0193e20ef3d5e4d01e974bfeac9c90
rishabhjaiswal2710 <rishabh.jaiswal@getonedirect.com>
2022-05-10 23:35:41 +0530

    list-added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react"
 import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
 import Hero from "./Components/Hero/hero"
-
+import List from "./Components/List/list"
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
       <Hero/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
     </div>
   );
 }
---
+++ src/Components/List/list.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./list.css"
+function list() {
+  return (
+    <div className='list-content'>
+        <p>Docker Package Repositories</p>
+        <p className='operate'>Operational</p>
+
+    </div>
+  )
+}
+
+export default list
